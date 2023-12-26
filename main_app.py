dd76ce41ffbb16a027fafe918e51b6b83219f4c1
Jordy-20035 <manyejordana@gmail.com>
2023-12-26 14:16:52 +0000

    modified:   main_app.py

--- main_app.py
+++ main_app.py
@@ -118,9 +118,9 @@ def main():
         # User-provided prompt
         if prompt := st.chat_input("How can i help ?"):
             # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"role": "user", "content": 'prompt'})
             # Append the dialogue history to the user's prompt
-            #dialogue_history = "\n".join([message["content"] for message in st.session_state.messages])
+            dialogue_history = "\n".join([message["content"] for message in st.session_state.messages])
             # Display user message in chat message container
             with st.chat_message("user"):
                 st.markdown(prompt)
